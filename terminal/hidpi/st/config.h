77b65e0144452564382257b6348d4ed13c8c7c50
Gobblescoque <skotekalx@gmail.com>
2020-11-12 15:33:14 -0700

    Changed some hidpi settings to make the laptop run a bit better

--- terminal/hidpi/st/config.h
+++ terminal/hidpi/st/config.h
@@ -5,8 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Fira Code:pixelsize=34:antialias=true:autohint=true";
-static char *font2[] = { "JoyPixels:pixelsize=30:antialias=true:autohint=true" };
+static char *font = "Fira Code:pixelsize=18:antialias=true:autohint=true";
+static char *font2[] = { "JoyPixels:pixelsize=14:antialias=true:autohint=true" };
 static int borderpx = 2;
 
 /*
